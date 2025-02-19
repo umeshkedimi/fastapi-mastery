b8d4e2380c8306703bc57f34c703fe65564fe6c0
Umesh Kedimi <umesh.kedimi@annalect.com>
2025-02-19 16:50:49 +0530

    Setting up pydantic

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "FastAPI Mastery Repo is Working!"}
+
+@app.get("/posts")
+def posts():
+    return {"data": "This is Post1"}
+
+@app.post("/createpost")
+def create_posts(new_post:Post):
+    print(new_post)
+    return {"new_post": "New post created", "info": f"Title-{new_post.title}. Content-{new_post.content}"}
